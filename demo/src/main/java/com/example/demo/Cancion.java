2740edd48a49ec035c553b9e8aed46536534fff0
Jd1ego <140413545+Jd1ego@users.noreply.github.com>
2024-10-27 18:57:54 -0500

    Crea, hace, modifica, empieza, absolutamente todo xd

--- demo/src/main/java/com/example/demo/Cancion.java
+++ demo/src/main/java/com/example/demo/Cancion.java
@@ -1,5 +1,35 @@
 package com.example.demo;
 
-public class Cancion {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public record Cancion (String id, String nombre, String genero, String artistaId){
+
+    public static final List<Cancion> canciones = new ArrayList<>(Arrays.asList(
+            new Cancion("cancion-1","Tabloid Junkie","Rock/pop","Artista-1"),
+            new Cancion("cancion-2","Countdown to extintion","Trash Metal","Artista-2"),
+            new Cancion("cancion-3","Given Up","Alternative Metal","Artista-3")
+    ));
+
+    public static Cancion getCancion(String id){
+        return canciones.stream()
+                .filter(cancion -> cancion.id.equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static Cancion addCancion(CancionInput cancionInput){
+        final Cancion cancion = new Cancion(
+                "cancion-"+canciones.size(),
+                cancionInput.nombre(),
+                cancionInput.genero(),
+                cancionInput.artistaId()
+
+        );
+        canciones.add(cancion);
+        return cancion;
+    }
+
 
 }
--- demo/src/main/java/com/example/demo/CancionController.java
+++ demo/src/main/java/com/example/demo/CancionController.java
@@ -1,4 +1,25 @@
 package com.example.demo;
 
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class CancionController {
+
+    @QueryMapping
+    public Cancion getCancion(@Argument String id) {
+        return Cancion.getCancion(id);
+    }
+    @MutationMapping
+    public Cancion addCancion(@Argument CancionInput cancionInput) {
+        return Cancion.addCancion(cancionInput);
+    }
+
+    @SchemaMapping
+    public Artista artista (Cancion cancion) {
+        return Artista.findById(cancion.artistaId());
+    }
 }
